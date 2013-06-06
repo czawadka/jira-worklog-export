2694fa59edc4646a950a094ca0eb00239572c1c4
czawadka <Cezary.Zawadka@ydp.eu>
2013-06-06 23:36:04 +0200

    append api path to base url
    Signed-off-by: czawadka <Cezary.Zawadka@ydp.eu>

--- src/main/ts/jira.ts
+++ src/main/ts/jira.ts
@@ -1,12 +1,26 @@
 ///<reference path="../../lib/ts/node.d.ts" />
 ///<reference path="http_client.ts" />
 import http_client = module('http_client');
+///<reference path="misc.ts" />
+import misc = module('misc');
 
 export class JiraRestClient {
     client: http_client.HttpClient;
 
     constructor(opts: http_client.HttpClientOptions) {
+        opts = this.appendApiPathToBaseUrl(opts);
+
         this.client = new http_client.HttpClient(opts);
+    }
+
+    private appendApiPathToBaseUrl(opts: http_client.HttpClientOptions): http_client.HttpClientOptions {
+        opts = misc.extend(opts);
+        if (opts.baseUrl[opts.baseUrl.length-1]!='/') {
+            opts.baseUrl += '/';
+        }
+        opts.baseUrl += 'rest/api/latest/';
+
+        return opts;
     }
 
     search(jql:string, startAt?: number, maxResults?: number, fields?: string, expand?: string): any {
