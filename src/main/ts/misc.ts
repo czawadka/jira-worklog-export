ce13a011328cdc464579e618bf70daeec96564a6
czawadka <Cezary.Zawadka@ydp.eu>
2013-06-10 10:46:11 +0200

    added for deep extend
    Signed-off-by: czawadka <Cezary.Zawadka@ydp.eu>

--- src/main/ts/misc.ts
+++ src/main/ts/misc.ts
@@ -1,9 +1,25 @@
-export function extend(src: any, dst?: any): any {
-    if (!dst) {
-        dst = {};
+import util = module("util");
+
+export function extend(source: any, deep?: bool, target?: any): any {
+
+    function createCompatibleTarget(source: any, target: any): any {
+        if (target) {
+            return target;
+        } else if (util.isArray(source)) {
+            return [];
+        } else {
+            return {};
+        }
     }
-    for(var key in src) {
-        dst[key] = src[key];
+
+    target = createCompatibleTarget(source, target);
+    for(var key in source) {
+        if (deep && typeof(target[key]) === "object") {
+            target[key] = createCompatibleTarget(source[key], target[key]);
+            extend(source[key], deep, target[key]);
+        } else if (typeof(source[key]) !== "undefined") {
+            target[key] = source[key];
+        }
     }
-    return dst;
+    return target;
 }
