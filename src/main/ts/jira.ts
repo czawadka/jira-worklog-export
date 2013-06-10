a9ec8fd167b1b46cee4bd31c32c0b76faffe54e1
czawadka <Cezary.Zawadka@ydp.eu>
2013-06-10 14:35:53 +0200

    added listWorklogs method and partial mapping TypeScript interfaces to Jira search method
    Signed-off-by: czawadka <Cezary.Zawadka@ydp.eu>

--- src/main/ts/jira.ts
+++ src/main/ts/jira.ts
@@ -1,19 +1,83 @@
 ///<reference path="../../lib/ts/node.d.ts" />
+import http = module('http');
 ///<reference path="http_client.ts" />
 import http_client = module('http_client');
 ///<reference path="misc.ts" />
 import misc = module('misc');
 
+export interface Issue {
+    key: string;
+    fields: Fields;
+}
+
+export interface Fields {
+    summary?: string;
+    worklog?: Worklogs;
+}
+
+export interface Worklogs {
+    total: number;
+    worklogs: Worklog[];
+}
+
+export interface Worklog {
+    author: User;
+    started: string;
+    timeSpentSeconds: number;
+}
+
+export interface User {
+    name: string;
+    emailAddress: string;
+    displayName: string;
+}
+
+export interface SearchResults {
+    startAt: number;
+    maxResults: number;
+    total: number;
+    issues: Issue[];
+}
+
+export function padString(s: string, width?: number, widthChar?: string) {
+    if (width && s.length < width) {
+        widthChar = widthChar || ' ';
+        for(var i = width - s.length; i>0; i--) {
+            s = widthChar + s;
+        }
+    }
+    return s;
+}
+export function fmtNumber(n: number, width?: number, widthChar?: string) {
+    var s: string = n.toString(10);
+    return padString(s, width, widthChar || '0');
+}
+
+export function jqlDate(date: Date): string {
+    return fmtNumber(date.getFullYear(), 4)
+        + "-"
+        + fmtNumber(date.getMonth()+1, 2)
+        + "-"
+        + fmtNumber(date.getDate(), 2)
+        + " "
+        + fmtNumber(date.getHours(), 2)
+        + ":"
+        + fmtNumber(date.getMinutes(), 2)
+//        + ":"
+//        + fmtNumber(date.getSeconds(), 2)
+        ;
+}
+
+
 export class JiraRestClient {
     client: http_client.HttpClient;
 
     constructor(opts: http_client.HttpClientOptions) {
-        opts = this.appendApiPathToBaseUrl(opts);
-
+        opts = JiraRestClient.appendApiPathToBaseUrl(opts);
         this.client = new http_client.HttpClient(opts);
     }
 
-    private appendApiPathToBaseUrl(opts: http_client.HttpClientOptions): http_client.HttpClientOptions {
+    private static appendApiPathToBaseUrl(opts: http_client.HttpClientOptions): http_client.HttpClientOptions {
         opts = misc.extend(opts);
         if (opts.baseUrl[opts.baseUrl.length-1]!='/') {
             opts.baseUrl += '/';
@@ -23,7 +87,7 @@ export class JiraRestClient {
         return opts;
     }
 
-    search(jql:string, startAt?: number, maxResults?: number, fields?: string, expand?: string): any {
+    search(jql:string, startAt?: number, maxResults?: number, fields?: string, expand?: string): Qpromise/* SearchResults */ {
         var requestOpts: http_client.HttpRequestOptions = {
             method: "GET",
             uri: "search",
@@ -35,6 +99,7 @@ export class JiraRestClient {
                 expand: expand
             }
         };
+
         var responsePromise: Qpromise = this.client.request(requestOpts);
 
         return responsePromise.then(
@@ -42,5 +107,76 @@ export class JiraRestClient {
                 return JSON.parse(data);
             }
         );
+    }
+
+    listWorklogs(dateFrom: Date, dateTo?: Date): Qpromise /* filteredIssues: Issue[] */ {
+
+        dateTo = dateTo || new Date();
+
+        var self: JiraRestClient = this,
+            jql: string = '',
+            isoDateFrom: string = dateFrom.toISOString(),
+            isoDateTo: string = dateTo.toISOString();
+
+        jql += "updated >= '" + jqlDate(dateFrom) + "'";
+        jql += " AND updated <= '" + jqlDate(dateTo) + "'";
+        jql += " ORDER BY key";
+        console.log(jql);
+
+        function filterIssues(issues: Issue[]): Issue[] {
+            return issues.filter(function(issue: Issue) {
+                //console.log('issue ' + JSON.stringify(issue));
+                issue.fields.worklog.worklogs = issue.fields.worklog.worklogs.filter(function(worklog: Worklog){
+                   return (worklog.started >= isoDateFrom || worklog.started <= isoDateTo);
+                });
+                return issue.fields.worklog.worklogs.length > 0;
+            });
+        }
+
+        function processWorklogChunk(searchResults: SearchResults): any /* Qpromise || filteredIssues: Issue[] */ {
+            console.log('processing issues ' + searchResults.startAt
+                + '-' + (searchResults.startAt + searchResults.issues.length - 1)
+                + '/' + searchResults.total);
+            var filteredIssues: Issue[] = filterIssues(searchResults.issues);
+
+            var nextStartAt: number = searchResults.startAt + searchResults.maxResults;
+            if (nextStartAt < searchResults.total) {
+                return nextWorklogChunk(nextStartAt, searchResults.maxResults)
+                    .then(function(nextFilteredIssues) {
+                        return filteredIssues.concat(nextFilteredIssues);
+                    })
+            } else {
+                return filteredIssues;
+            }
+        }
+
+        var initialBulkSize = 100,
+            initialStartAt = 300;
+
+        function nextWorklogChunk(startAt: number, bulkSize: number): Qpromise /* filteredIssues: Issue[] */ {
+            console.log("Search at "+(startAt+1)+"-"+(startAt+bulkSize));
+            return self.search(jql, startAt, bulkSize, "summary,worklog,-attachment", "")
+                .then(
+                    processWorklogChunk,
+                    function(response: http.ClientResponse) {
+                        if (response.statusCode == 500) {
+                            // assume Jira crashed due to some problem with particular issue
+                            // try to find that issue and skip it
+                            if (bulkSize == 1) {
+                                console.error("FOUND culpring at "+(startAt+1)+". Skipping it.");
+                                return nextWorklogChunk(startAt + 1, initialBulkSize);
+                            } else {
+                                console.error("Detected issue in "+(startAt+1)+"-"+(startAt+bulkSize)+". Narrowing the problem.");
+                                var newBulkSize = Math.ceil(bulkSize/3);
+                                return nextWorklogChunk(startAt, newBulkSize);
+                            }
+                        } else {
+                            throw response;
+                        }
+                    });
+        }
+
+        return nextWorklogChunk(initialStartAt, initialBulkSize);
+
     }
 }
--- src/main/ts/start.ts
+++ src/main/ts/start.ts
@@ -8,10 +8,18 @@ var clientOptions = {
 };
 var jiraClient: jira.JiraRestClient = new jira.JiraRestClient(clientOptions);
 
-jiraClient.search("project = PSS AND resolution = Unresolved", 0, 3, "*all")
-    .then(function(searchResult) {
-        console.log("success");
-    }, null, function(event) {
-        console.log("notify: "+event.event);
+var dateFrom: Date = new Date(2013,4,1),
+    dateTo: Date = new Date(2013,5,1);
+
+jiraClient
+    .listWorklogs(dateFrom, dateTo)
+    .then(function(issues: jira.Issue[]) {
+
+        issues.forEach(function(issue: jira.Issue) {
+            issue.fields.worklog.worklogs.forEach(function(worklog: jira.Worklog){
+                console.log(issue.key+","+worklog.author.name+","+worklog.started+","+worklog.timeSpentSeconds);
+            });
+        });
+
     })
     .done();
